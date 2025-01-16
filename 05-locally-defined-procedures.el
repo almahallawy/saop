207eefa5dadbdc37448df8b31537f4647e4b1f9d
Khaled Almahallawy <almahallawy@gmail.com>
2025-01-16 09:14:48 -0800

    elisp code up to ex 5.9

--- 05-locally-defined-procedures.el
+++ 05-locally-defined-procedures.el
@@ -329,3 +329,242 @@
 (list-ref '((1 2) (3 4) (5 6)) 1)q
 (list-ref '() 0)
 
+;;5.3 Symbolic manipulation of Polynomials
+
+; - Program 5.14, pg. 151 -
+;; The five basic defintion (version I)
+
+(defun sub1 (n)
+  (1- n))
+
+(defun list-of-zeros (n)
+  (cond
+   ((zerop n) '())
+   (t (cons 0 (list-of-zeros (sub1 n))))))
+
+(list-of-zero 5)
+
+(setq the-zero-poly '(0))
+
+(defun degree (poly)
+  (sub1 (length poly)))
+
+(defun leading-coef (poly)
+  (car poly))
+
+(defun rest-of-poly (poly)
+  (cond
+   ((zerop (degree poly)) the-zero-poly)
+   ((zerop (leading-coef (cdr poly)))
+    (rest-of-poly (cdr poly)))
+   (t (cdr poly))))
+
+(defun poly-cons (deg coef poly)
+  (let ((deg-p (degree poly)))
+    (cond
+     ((and (zerop deg) (equal poly the-zero-poly)) (list coef))
+     ((< deg-p deg)
+      (if (zerop coef)
+          poly
+        (cons coef 
+              (append (list-of-zeros (sub1 (- deg deg-p))) 
+                      poly))))
+     (t (error "poly-cons: Degree too high in %s" poly)))))
+
+;; - End Program -
+
+
+; - Program 5.15, pg. 153 -
+;; The five basic defintion (version II)
+
+(setq the-zero-poly '((0 0)))
+
+(defun degree (poly)
+  (caar poly))
+
+(defun leading-coef (poly)
+  (cadar poly))
+
+(defun rest-of-poly (poly)
+  (if (null (cdr poly))
+      the-zero-poly
+    (cdr poly)))
+
+(defun poly-cons (deg coef poly)
+  (let ((deg-p (degree poly)))
+    (cond 
+     ((and (zerop deg) (equal poly the-zero-poly))
+      (list (list deg coef)))
+     ((< deg-p deg)
+      (if (zerop coef) 
+          poly
+        (cons (list deg coef) poly)))
+     (t (error "poly-cons: degree too high in %s" poly)))))
+
+;; - End Program -
+
+; - Program 5.6, pg. 144 -
+
+(defun zero-poly? (poly)
+  (and (zerop (degree poly)) (zerop (leading-coef poly))))
+
+; - End Program -
+
+; - Program 5.7, pg. 145 -
+
+(defun make-term (deg coef)
+  (poly-cons deg coef the-zero-poly)) 
+
+; - End Program -
+
+; - Program 5.8, pg. 145 -
+
+(defun leading-term (poly)
+  (make-term (degree poly) (leading-coef poly)))
+
+; - End Program -
+
+; - Program 5.9, pg. 146 -
+
+(defun p+ (poly1 poly2)
+  (cond
+   ((zero-poly? poly1) poly2)
+   ((zero-poly? poly2) poly1)
+   (t (let ((n1 (degree poly1))
+               (n2 (degree poly2))
+               (a1 (leading-coef poly1))
+               (a2 (leading-coef poly2))
+               (rest1 (rest-of-poly poly1))
+               (rest2 (rest-of-poly poly2)))
+           (cond 
+            ((> n1 n2) (poly-cons n1 a1 (p+ rest1 poly2)))
+            ((< n1 n2) (poly-cons n2 a2 (p+ poly1 rest2)))
+            (t
+             (poly-cons n1 (+ a1 a2) (p+ rest1 rest2)))))))) 
+
+; - End Program -
+
+; - Program 5.10, pg. 148 -
+
+(setq p* 
+  (letrec 
+    ((t* (lambda (trm poly)
+           (if (zero-poly? poly)
+               the-zero-poly
+               (poly-cons 
+                 (+ (degree trm) (degree poly))
+                 (* (leading-coef trm) (leading-coef poly))
+                 (funcall t* trm (rest-of-poly poly)))))))
+    (lambda (poly1 poly2)
+      (letrec
+        ((p*-helper (lambda (p1)
+                      (if (zero-poly? p1)
+                          the-zero-poly
+                          (p+ (funcall t* (leading-term p1) poly2)
+                              (funcall p*-helper (rest-of-poly p1)))))))
+        (funcall p*-helper poly1)))))
+
+;;Organize the function little bit different than the book
+;;move p*-helper to letrec of t*
+
+(defun p* (poly1 poly2) 
+  (letrec 
+      ((t* (lambda (trm poly)
+             (if (zero-poly? poly)
+		 the-zero-poly
+	       (poly-cons 
+                (+ (degree trm) (degree poly))
+                (* (leading-coef trm) (leading-coef poly))
+                (funcall t* trm (rest-of-poly poly))))))
+       (p*-helper (lambda (p1)
+                    (if (zero-poly? p1)
+                        the-zero-poly
+                      (p+ (funcall t* (leading-term p1) poly2)
+                          (funcall p*-helper (rest-of-poly p1)))))))
+    (funcall p*-helper poly1)))
+
+;; - End Program -
+;; - Program 5.11, pg. 148 -
+
+(defun negative-poly (poly) 
+  (let ((poly-negative-one (make-term 0 -1)))
+    (p* poly-negative-one poly)))
+
+; - End Program -
+
+; - Program 5.12, pg. 148 -
+
+(defun p- (poly1 poly2)
+  (p+ poly1 (negative-poly poly2)))  
+
+; - End Program -
+
+; - Program 5.13, pg. 150 -
+
+(defun poly-value (poly num)
+  (letrec 
+      ((pvalue (lambda (p)
+                 (let ((n (degree p)))
+                   (if (zerop n) 
+                       (leading-coef p)
+                     (let ((rest (rest-of-poly p)))
+                       (if (< (degree rest) (sub1 n))
+                           (funcall pvalue (poly-cons
+                                    (sub1 n)
+                                    (* num (leading-coef p))
+                                    rest))
+                         (funcall pvalue (poly-cons 
+                                  (sub1 n)
+                                  (+ (* num (leading-coef p))
+                                     (leading-coef rest))
+                                  (rest-of-poly rest))))))))))
+    (funcall pvalue poly)))
+
+
+;;
+;;3x^4 + 5x^2 + 12
+(setq p1
+  (poly-cons 4 3
+	     (poly-cons 2 5
+			(poly-cons 0 12 the-zero-poly))))
+
+;;7x^5 + 6x4 - x^2 + 11x - 15
+(setq p2
+  (poly-cons 5 7
+	     (poly-cons 4 6
+			(poly-cons 2 -1
+				   (poly-cons 1 11
+					      (poly-cons 0 -15 the-zero-poly))))))
+
+;; p1+p2 = 7x^5 + 9x^4 + 4x^2 + 11x-3
+(poly-value (p+ p1 p2) 1) ;;7+9+4+11-3=28
+
+
+;;Ex5.9
+;;p1(x) = 5x^4 - 7x^3 +        2x - 4
+;;p2(x) =         x^3 + 6x^2 - 3x
+
+(setq p1
+  (poly-cons 4 5
+	     (poly-cons 3 -7
+			(poly-cons 1 2
+				   (poly-cons 0 -4 the-zero-poly)))))
+
+(setq p2
+  (poly-cons 3 1
+	     (poly-cons 2 6
+			(poly-cons 1 -3 the-zero-poly))))
+
+;;p1+p2 = 5x^4 - 6x^3 + 6x^2 - x - 4
+(poly-value (p+ p1 p2) 1) ;; 5-6+6-1-4 = 0
+
+;;p1-p2 = 5x^4 - 8x^3 - 6x^2 + 5x - 4
+(poly-value (p- p1 p2) 1) ;; 5-8-6+5-4 = -8
+
+;;p1*p2 =  5x^7 + 23x^6 - 57x^5 + 23x^4 + 8x^3 - 30x^2 + 12x
+(poly-value (p* p1 p2) 1) ;; = 5 + 23 - 57 + 23 + 8 - 30 + 12 = -16
+
+(poly-value p1 -1) ;; 5 + 7 - 2 -4 = 6
+(poly-value p1 2) ;; 5*16 - 7*8 + 2*2 -4 = 24
+(poly-value p2 0) ;; 0
+(poly-value p2 -2) ;; -8 + 6*4 - 3* -2 = 22
